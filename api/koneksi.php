45e86ab335bcea105d95816eee0e879626461d32
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-06-18 15:54:52 +0700

    edit koneksi

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,5 +1,4 @@
 <?php 
-
 // Buat koneksi ke database TIDB
 $host     = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
 $username = "3ZRN5c6PUXtTqvf.root";
@@ -7,14 +6,17 @@ $password = "mWYBDrZ8ogUT5beM";
 $database = "jelajahjatim";
 $port     = 4000;
 
+// Variabel ini yang tadi hilang/terlewat! 
+// Pastikan file isrgrootx1.pem ada di dalam folder D:\UNS\backend\jelajahWisata\
+$cert_path = dirname(__DIR__) . '/isrgrootx1.pem';
+
 // Deteksi koneksi
 try {
-    // Spasi di setelah titik koma dihapus agar parser PHP membaca DSN dengan sempurna
     $dsn = "mysql:host=$host;port=$port;dbname=$database;ssl-mode=VERIFY_IDENTITY";
     
     $pdo = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Lempar pesan jika terjadi error 
-        Pdo\Mysql::ATTR_SSL_CA => dirname(__DIR__) . '/isrgrootx1.pem',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+        PDO::MYSQL_ATTR_SSL_CA => $cert_path, // Memanggil variabel $cert_path di sini
     ]);
     
     // echo "Koneksi berhasil!"; 

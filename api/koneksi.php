0be9257b717919e5c26d07f971590b7c8da8c194
neehanr29-bit <neehan.r29@gmail.com>
2026-06-18 15:37:40 +0700

    edit koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,21 +1,24 @@
 <?php 
 
-//Buat koneksi ke database TIDB
+// Buat koneksi ke database TIDB
 $host     = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
 $username = "3ZRN5c6PUXtTqvf.root";
 $password = "mWYBDrZ8ogUT5beM";
 $database = "jelajahjatim";
 $port     = 4000;
 
-
-//Deteksi koneksi
-try{
-    $dsn = "mysql:host=$host; port=$port; dbname=$database; ssl-mode=VERIFY_IDENTITY";
+// Deteksi koneksi
+try {
+    // Spasi di setelah titik koma dihapus agar parser PHP membaca DSN dengan sempurna
+    $dsn = "mysql:host=$host;port=$port;dbname=$database;ssl-mode=VERIFY_IDENTITY";
+    
     $pdo = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //lempar pesan jika terjadi error 
-        PDO::MYSQL_ATTR_SSL_CA => dirname(__DIR__) . '/isrgrootx1.pem',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Lempar pesan jika terjadi error 
+        Pdo\Mysql::ATTR_SSL_CA => dirname(__DIR__) . '/isrgrootx1.pem',
     ]);
-    // echo "Koneksi berhasil!";
+    
+    // echo "Koneksi berhasil!"; 
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
-}
+} 
+?>
